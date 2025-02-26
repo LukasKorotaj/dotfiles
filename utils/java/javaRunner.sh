bb82ad51233b10c432337b20c23676f8470d5d24
Lukas Korotaj <lu0026ko-s@student.lu.se>
2025-02-26 08:10:27 +0100

    modified javaRunner.sh so it takes arguments

--- utils/java/javaRunner.sh
+++ utils/java/javaRunner.sh
@@ -6,13 +6,14 @@ BIN_DIR="./bin"
 
 # Check if the user provided a main class argument
 if [ -z "$1" ]; then
-    echo "Usage: $0 <MainClass>"
-    echo "Example: $0 game.TakePinsGame"
+    echo "Usage: $0 <MainClass> [java arguments...]"
+    echo "Example: $0 game.TakePinsGame arg1 arg2"
     exit 1
 fi
 
 # The main class is passed as the first argument
 MAIN_CLASS=$1
+shift  # Tar bort första argumentet (main-klassen), så att "$@" nu innehåller övriga argument
 
 # Find all .java files in the src/ directory and store them in a variable
 JAVA_FILES=$(find $SRC_DIR -name "*.java")
@@ -25,9 +26,9 @@ javac -d $BIN_DIR $JAVA_FILES
 if [ $? -eq 0 ]; then
     echo "Compilation successful."
 
-    # Run the compiled class from the bin/ directory
+    # Run the compiled class from the bin/ directory, vidarebefordra eventuella extra argument
     echo "Running Java program..."
-    java -cp $BIN_DIR $MAIN_CLASS
+    java -cp $BIN_DIR $MAIN_CLASS "$@"
 else
     echo "Compilation failed."
     exit 1
