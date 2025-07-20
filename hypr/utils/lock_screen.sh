742b8b5c1bc7acecd21252e99b744036146af691
Lukas Korotaj <lu0026ko-s@student.lu.se>
2025-07-20 18:50:57 +0200

    Final config

---
+++ hypr/utils/lock_screen.sh
@@ -0,0 +1,1 @@
+swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color ff69b4 --key-hl-color fc0fc0 --line-color 00000000 --inside-color 00000088 --separator-color 00000000
