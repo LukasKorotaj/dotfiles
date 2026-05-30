4bdb89fec547137062aacf369fefd5bb94c5ec3f
Lukas Korotaj <lu0026ko-s@student.lu.se>
2026-05-30 14:55:44 +0200

    Multi device monitor config

--- dot_config/hypr/lua/config/monitors.lua
+++ dot_config/hypr/lua/config/monitors.lua
@@ -1,8 +1,23 @@
-hl.env("GDK_SCALE", "1")
+local file = io.open("/etc/hostname", "r")
+local hostname = ""
+if file then
+	hostname = file:read("*l"):gsub("%s+", "")
+	file:close()
+end
 
-hl.monitor({
-	output = "DP-2",
-	mode = "1920x1080@144",
-	position = "auto",
-	scale = 1.0,
-})
+if hostname == "archlukas" then
+	hl.env("GDK_SCALE", "1")
+
+	hl.monitor({
+		output = "DP-2",
+		mode = "1920x1080@144",
+		position = "auto",
+		scale = 1.0,
+	})
+elseif hostname == "elitearch" then
+	hl.env("GDK_SCALE", "1.2")
+
+	hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1.2 })
+
+	hl.monitor({ output = "HDMI-A-1", mode = "preferred", position = "auto-up", scale = 1.2 })
+end
